80fbe50d9fc81131dfd8c380ba3df60df604bde4
TinuClara <120931103+TinuClara@users.noreply.github.com>
2023-09-29 08:39:19 +0530

    registration form and login form completed

--- bookshell/store/urls.py
+++ bookshell/store/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('', views.index,name="index")
+    path('', views.index,name="index"),
+    path('register', views.register,name="register"),
+    path('login', views.loginn,name="loginn"),
 ]
