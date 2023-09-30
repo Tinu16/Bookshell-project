f8a797d1a1fe9986d26e2f3fe916a3d15cdb45c6
TinuClara <120931103+TinuClara@users.noreply.github.com>
2023-09-30 08:22:43 +0530

    seller account created

--- bookshell/store/urls.py
+++ bookshell/store/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.index,name="index"),
     path('register', views.register,name="register"),
     path('login', views.loginn,name="loginn"),
+    path('seller_register', views.seller_register,name="seller_register"),
 ]
--- bookshell/store/views.py
+++ bookshell/store/views.py
@@ -7,3 +7,5 @@ def register(request):
     return render(request,'register.html')
 def loginn(request):
     return render(request,'login.html')
+def seller_register(request):
+    return render(request,'seller_register.html')
