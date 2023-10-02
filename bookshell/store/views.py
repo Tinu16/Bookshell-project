4f18b5fe6b4fc0493d7ba11b849cda8bd2fb2418
TinuClara <120931103+TinuClara@users.noreply.github.com>
2023-10-02 16:14:33 +0530

    account creation validation almost completed

--- bookshell/store/views.py
+++ bookshell/store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -9,3 +12,8 @@ def loginn(request):
     return render(request,'login.html')
 def seller_register(request):
     return render(request,'seller_register.html')
+def pan_verification(request):
+    return render(request,'pan_verification.html')
+def selling_homepage(request):
+    return render(request,'selling_homepage.html')
+
