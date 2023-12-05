37744a6734e476c6cb74a1f969425fc697e943a3
Tinu16 <141432362+Tinu16@users.noreply.github.com>
2023-12-05 23:52:33 +0530

    Add files via upload

--- cart.php
+++ cart.php
@@ -3,111 +3,88 @@ session_start();
 include("dbcon.php");
 include("config.php");
 include("authentication.php");
-include("includes/header.php");
-include("includes/customer_sidebar.php");
-include("includes/topbar.php");
+
 include("message.php");
-$status="";
-if (isset($_POST['action']) && $_POST['action']=="remove"){
-if(!empty($_SESSION["shopping_cart"])) {
-    foreach($_SESSION["shopping_cart"] as $key => $value) {
-      if($_POST["code"] == $key){
-      unset($_SESSION["shopping_cart"][$key]);
-      $status = "<div class='box' style='color:red;'>
-      Product is removed from your cart!</div>";
+
+if (isset($_SESSION["auth_user"]) && isset($_POST["book_id"])) {
+    $user = $_POST['useremail'];
+    $bid = $_POST['book_id'];
+    $bname = $_POST['bookname'];
+    $bprice = $_POST['amount'];
+    $bimg = $_POST['bookimg'];
+    $bqty = 1;
+    $date = date('Y-m-d h:i:s');
+
+    // Using prepared statements to prevent SQL injection
+    $stmt = $con->prepare('SELECT book_id FROM tbl_cart WHERE book_id=? AND user_email=?');
+    $stmt->bind_param('ss', $bid, $user);
+    $stmt->execute();
+    $res = $stmt->get_result();
+
+    // Fetch the associative array
+    $r = $res->fetch_assoc();
+
+    // Check if 'book_id' exists in $r
+    if (!isset($r['book_id'])) {
+        $query = "INSERT INTO `tbl_cart`(`user_email`, `book_id`, `quantity`, `cart_date`) VALUES (?, ?, ?, ?)";
+        $stmt = $con->prepare($query);
+        $stmt->bind_param('siss', $user, $bid, $bqty, $date);
+        $sql_run = $stmt->execute();
+
+        if ($sql_run) {
+            echo '
+         <div class="alert alert-success alert-dismissible">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <strong> Book added to your cart.</strong>
+          </div>';
+        } else {
+            // Output the detailed error message for debugging
+            echo '
+         <div class="alert alert-danger alert-dismissible">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <strong> Book is not added to your cart. Please try again later.</strong>
+              <br> Error: ' . $stmt->error;
+            echo '</div>';
+
+            // Log the error to a file for further investigation
+            error_log("SQL Error: " . $stmt->error, 0);
+        }
+    } else {
+        echo '
+      <div class="alert alert-info alert-dismissible">
+           <button type="button" class="close" data-dismiss="alert">&times;</button>
+           <strong> Book is already added to your cart.</strong>
+       </div>';
+    }
+}
+
+if (isset($_GET['cartItem']) && isset($_GET['cartItem']) == 'cart_item') {
+    $stmt = $con->prepare('SELECT * FROM tbl_cart');
+    $stmt->execute();
+    $stmt->store_result();
+    $rows = $stmt->num_rows;
+
+    echo $rows;
+  }
+  // Remove single items from cart
+	if (isset($_GET['remove'])) {
+        $id = $_GET['remove'];
+  
+        $stmt = $con->prepare('DELETE FROM tbl_cart WHERE id=?');
+        $stmt->bind_param('i',$id);
+        $stmt->execute();
+  
+        $_SESSION['showAlert'] = 'block';
+        $_SESSION['message'] = 'Item removed from the cart!';
+        header('location:cartadd.php');
       }
-      if(empty($_SESSION["shopping_cart"]))
-      unset($_SESSION["shopping_cart"]);
-      }		
-}
-}
-
-if (isset($_POST['action']) && $_POST['action']=="change"){
-  foreach($_SESSION["shopping_cart"] as &$value){
-    if($value['code'] === $_POST["code"]){
-        $value['quantity'] = $_POST["quantity"];
-        break; // Stop the loop after we've found the product
-    }
-}
-  	
-}
-?>
-<div class="cart">
-<?php
-if(isset($_SESSION["shopping_cart"])){
-    $total_price = 0;
-?>	
-<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-    <tbody>
-        <tr>
-            <td></td>
-            <td>ITEM NAME</td>
-            <td>QUANTITY</td>
-            <td>UNIT PRICE</td>
-            <td>ITEMS TOTAL</td>
-        </tr>	
-<?php		
-foreach ($_SESSION["shopping_cart"] as $product)
-    {
-    ?>
-        <tr>
-            <td><img src='images/<?php echo $product["image"]; ?>' width="50" height="40" /></td>
-            <td><?php echo $product["name"]; ?><br />
-            <form method='post' action=''>
-            <input type='hidden' name='code' value="<?php echo $product["code"]; ?>" />
-            <input type='hidden' name='action' value="remove" />
-            <button type='submit' class='remove'>Remove Item</button>
-            </form>
-            </td>
-            <td>
-                <form method='post' action=''>
-                <input type='hidden' name='code' value="<?php echo $product["code"]; ?>" />
-                <input type='hidden' name='action' value="change" />
-                <select name='quantity' class='quantity' onChange="this.form.submit()">
-                <option <?php if($product["quantity"]==1) ?>
-                value="1">1</option>
-                <option <?php if($product["quantity"]==2)?>
-                value="2">2</option>
-                <option <?php if($product["quantity"]==3) ?>
-                value="3">3</option>
-                <option <?php if($product["quantity"]==4)?>
-                value="4">4</option>
-                <option <?php if($product["quantity"]==5)?>
-                value="5">5</option>
-                </select>
-                </form>
-            </td>
-            <td><?php echo "Rs.".$product["price"]; ?></td>
-            <td><?php echo "Rs.".$product["price"]*$product["quantity"]; ?></td>
-        </tr>
-<?php
-    $total_price += ($product["price"]*$product["quantity"]);
-    }
-?>
-<tr>
-    <td colspan="5" align="right"><strong>TOTAL: <?php echo "Rs.".$total_price; ?></strong></td>
-</tr>
-    </tbody>
-</table>		
-<?php
-}
-else
-    {
-        ?>
-        <center>
-        <img src="img/empty_cart.png" height="250px" width="250px" class="img-fluid animated" alt="">
-        <?php
-	echo "<h3>Your cart is empty!</h3>";
-	}
-?>
-</center>
-</div>
-
-<div style="clear:both;"></div>
-
-<div class="message_box" style="margin:10px 0px;">
-<?php echo $status; ?>
-</div>
-<?php 
-    include("includes/footer.php");
+  
+      // Remove all items at once from cart
+      if (isset($_GET['clear'])) {
+        $stmt = $con->prepare('DELETE FROM tbl_cart');
+        $stmt->execute();
+        $_SESSION['showAlert'] = 'block';
+        $_SESSION['message'] = 'All Item removed from the cart!';
+        header('location:cartadd.php');
+      }
 ?>
